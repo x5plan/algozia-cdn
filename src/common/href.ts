9d896e83cedd6f88df5fefe038d5035425d2ba57
Antares <antares.mi@outlook.com>
2024-10-07 00:30:26 +0800

    fix a11y

--- src/common/href.ts
+++ src/common/href.ts
@@ -1,13 +1,20 @@
 $(function () {
-    $(document).on("click", "a[href-post]", (e) => {
-        e.preventDefault();
+    $("a[href-post]")
+        .on("click", (e) => {
+            e.preventDefault();
 
-        const form = document.createElement("form");
-        form.style.display = "none";
-        form.method = "post";
-        form.action = $(e.currentTarget).attr("href-post")!;
-        form.target = "_self";
-        document.body.appendChild(form);
-        form.submit();
-    });
+            const form = document.createElement("form");
+            form.style.display = "none";
+            form.method = "post";
+            form.action = $(e.currentTarget).attr("href-post")!;
+            form.target = "_self";
+            document.body.appendChild(form);
+            form.submit();
+        })
+        .on("keydown", (e) => {
+            if (e.key === "Enter" || e.key === " ") {
+                e.preventDefault();
+                $(e.currentTarget).trigger("click");
+            }
+        });
 });
