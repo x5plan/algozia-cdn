2ffecea996da04b9d75e2b8169d0afa78db25974
Antares <antares.mi@outlook.com>
2024-10-07 20:53:09 +0800

    add vite for sub pages (#1)

---
+++ pages/judge-setting/app.tsx
@@ -0,0 +1,11 @@
+import React from "preact/compat";
+
+export const App: React.FC = () => {
+    return (
+        <div >
+            <div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
